c0160958e4aafb87c4453c7f9d5f3dd688c80c89
javaNoviceProgrammer <meisam@192.168.2.14>
2018-07-15 17:32:40 -0500

    minor change

--- src/gds/layout/executable/Create_New_Cell.java
+++ src/gds/layout/executable/Create_New_Cell.java
@@ -11,26 +11,24 @@ import gds.layout.cells.Cell;
 
 public class Create_New_Cell implements Experiment {
 
-	Cell cell ;
+	Cell cell;
 
-	public Create_New_Cell(
-			@ParamName(name="Design your Cell") Cell cell
-			){
-		this.cell = cell ;
+	public Create_New_Cell(@ParamName(name = "Design your Cell") Cell cell) {
+		this.cell = cell;
 	}
 
 	@Override
 	public void run(AbstractResultsManager man, AbstractResultsDisplayer dis) throws WrongExperimentException {
 		// creating the gds file
-		PythonCompiler p = new PythonCompiler(cell) ;
+		PythonCompiler p = new PythonCompiler(cell);
 		p.compile();
-		AbstractResultsDisplayer.showGUI = false ;
+		AbstractResultsDisplayer.showGUI = false;
 	}
-	
+
 	public static void main(String[] args) {
 		String pacakgeString = "gds";
 		String classString = "gds.layout.executable.Create_New_Cell";
 		ExperimentConfigurationCockpit.execute(new String[] { "-p", pacakgeString, "-c", classString }, true);
 	}
-	
+
 }
