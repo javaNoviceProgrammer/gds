08a5a04fc304044962f3083f6590c4a8bf9e15bc
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-12-13 11:07:56 -0600

    minor changes.

--- src/gds/io/PythonCompiler.java
+++ src/gds/io/PythonCompiler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class PythonCompiler {
 
 	static String fileName ;
+	static String pythonVersion = "3.5" ;
 
 	public PythonCompiler(String fileName){
 		PythonCompiler.fileName = fileName ;
@@ -16,6 +17,10 @@ public class PythonCompiler {
 	public PythonCompiler(Cell cell){
 		PythonCompiler.fileName = cell.cellPath ;
 	}
+	
+	public void setPythonVersion(String version) {
+		pythonVersion = version ;
+	}
 
 	public void compile(){
 		String command = "" ;
@@ -23,8 +28,23 @@ public class PythonCompiler {
 			command = "/Library/Frameworks/Python.framework/Versions/3.5/bin/python3 " + fileName + ".py" ;
 		}
 		else if(OSDetector.isWindows()){
-//			command = "python " + fileName + ".py" ;
 			command = "python " + "\"" + fileName + ".py" + "\"" ;
+		}
+		try {
+			Runtime.getRuntime().exec(command) ;
+			System.gc();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void compile(Cell cell){
+		String command = "" ;
+		if(OSDetector.isMac()){
+			command = "/Library/Frameworks/Python.framework/Versions/" + pythonVersion + "/bin/python3 " + cell.cellPath + ".py" ;
+		}
+		else if(OSDetector.isWindows()){
+			command = "python " + "\"" + cell.cellPath + ".py" + "\"" ;
 		}
 		try {
 			Runtime.getRuntime().exec(command) ;
